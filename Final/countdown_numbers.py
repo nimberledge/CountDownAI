fb739de634c8337d6c4fbd7ebffaefee90760d69
akhil <akhil2@illinois.edu>
2018-11-06 17:41:29 -0600

    shell script commit

--- Final/countdown_numbers.py
+++ Final/countdown_numbers.py
@@ -16,43 +16,45 @@ def reach(target, num_set):
             temp.append(k)
             r = reach(target, temp)
             if (r[0]):
-                return (True, (str(num1) + '+' + str(num2) + ' set:' + str(temp) + "\t" +  r[1] + '\t\t'))
+                return (True, ('\n' + str(num1) + '+' + str(num2) + ' = ' + str(num1 + num2) + '\nset:' + str(temp) + "\t" +  r[1] + '\t\t'))
             temp.remove(k)
             k = num1 - num2
             temp.append(k)
             r = reach(target, temp)
             if (r[0]):
-                return (True, (str(num1) + '-' + str(num2) + ' set:' + str(temp) + "\t" +  r[1] + '\t\t'))
+                return (True, ('\n' + str(num1) + '-' + str(num2) + ' = ' + str(num1 - num2) + '\nset:' + str(temp) + "\t" +  r[1] + '\t\t'))
             temp.remove(k)
             k = num1 * num2
             temp.append(k)
             r = reach(target, temp)
             if (r[0]):
-                return (True, (str(num1) + '*' + str(num2) + ' set:' + str(temp) + "\t" +  r[1] + '\t\t'))
+                return (True, ('\n' + str(num1) + '*' + str(num2) + ' = ' + str(num1 * num2) + '\nset:' + str(temp) + "\t" +  r[1] + '\t\t'))
             if (num2 == 0):
                 continue
             if (num1 % num2) == 0:
                 temp.remove(k)
-                k = num1 / num2
+                k = num1 // num2
                 temp.append(k)
                 r = reach(target, temp)
                 if (r[0]):
-                    return (True, (str(num1) + '/' + str(num2) + ' set:' + str(temp) + "\t" + r[1] + '\t\t'))
+                    return (True, (('\n' + str(num1) + '/' + str(num2) + ' = ' + str(num1 // num2) + '\nset:' + str(temp) + "\t" +  r[1] + '\t\t'))
     return (False, '')
 
 def main():
-    target = 952
-    num_set = [42, 65, 3, 19, 952]
-    print (num_set)
-    print ("Target:", target)
-    start = time.time()
-    b = reach(target, num_set)
-    t = time.time() - start
-    if b[0]:
-        print (b[1])
-    else:
-        print ("na u suck")
-    print ("Time taken:", t)
+    while True:
+        print ("Enter numbers")
+        num_set = [int(j) for j in input().split()]
+        print (num_set)
+        print ("Target:")
+        target = int(input())
+        start = time.time()
+        b = reach(target, num_set)
+        t = time.time() - start
+        if b[0]:
+            print (b[1])
+        else:
+            print ("na u suck")
+        print ("Time taken:", t)
 
 if __name__ == '__main__':
     main()
